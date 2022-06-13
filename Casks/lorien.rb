4a1b025b8aaa9f4c8e93e0774ba4dec5cd0b957f
Michael Beasley <michael.beasley@alvaria.com>
2022-06-13 10:14:30 -0500

    test cask

---
+++ Casks/lorien.rb
@@ -0,0 +1,21 @@
+cask "4k-stogram" do
+  version "4.3.2"
+  sha256 "67b26ce34150ca58bdffeed81c3917d42a7d33b2fd5994b73901190e19feff79"
+
+  url "https://dl.4kdownload.com/app/4kstogram_#{version}.dmg"
+  name "4K Stogram"
+  desc "Download Instagram photos, accounts, hashtags and locations"
+  homepage "https://www.4kdownload.com/products/product-stogram"
+
+  livecheck do
+    url "https://www.4kdownload.com/downloads"
+    regex(%r{href=.*?/4kstogram[._-]?v?(\d+(?:\.\d+)+)\.dmg}i)
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "4K Stogram.app"
+
+  zap trash: "~/Pictures/4K Stogram"
+end
