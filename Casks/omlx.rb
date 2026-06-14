763c996f05ec2aa5b076270b5eda7adec54555bb
Michael Beasley <youvegotmoxie@gmail.com>
2026-06-13 16:46:58 -0800

    add cask for oMLX
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

---
+++ Casks/omlx.rb
@@ -0,0 +1,25 @@
+# https://github.com/jundot/omlx/releases
+cask "omlx" do
+  version "0.4.3"
+  sha256 "sha256:2a5cb5c76505998f0725b1663202e3c8d8980bd3ca805abcb44cd2c2422f57f4"
+
+  url "https://github.com/jundot/omlx/releases/download/v#{version}/oMLX-#{version}-macos26-27.dmg"
+  name "oMLX"
+  desc "oMLX is a macOS app for LLM inference"
+  homepage "https://omlx.ai"
+
+
+  livecheck do
+    url :url
+  end
+
+  depends_on "macos: >= :sequioa"
+  auto_updates true
+
+  app "oMLX.app"
+
+  zap trash: [
+    "/Applications/oMLX.app",
+    "~/Library/Application Support/oMLX",
+  ]
+end
